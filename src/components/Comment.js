3f760bbe78995dc5398e2bf039e258ec02a7d1b8
Jaidan Brown <jaidanbrown1999@gmail.com>
2022-01-23 16:01:49 +0000

    Finsihed main styling for comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { FaPlus, FaMinus, FaArrowCircleLeft } from 'react-icons/fa'
+import ProfileImage from '../assets/images/avatars/image-amyrobson.png'
+import IconPlus from '../assets/images/icon-plus.svg'
+import IconMinus from '../assets/images/icon-minus.svg'
+import IconReply from '../assets/images/icon-reply.svg'
 
 const Comment = () => {
 	return (
 		<div className='comment-container'>
 			<div className='vote'>
-				<div className='plus'>
-					<FaPlus />
+				<div className='icon-small plus'>
+					<img src={IconPlus} />
 				</div>
 				<div className='count'>10</div>
-				<div className='minus'>
-					<FaMinus />
+				<div className='icon-small minus'>
+					<img src={IconMinus} />
 				</div>
 			</div>
 			<div className='comment-main'>
 				<div className='comment-meta'>
-					<img src='' alt='' />
+					<img className='comment-image' src={ProfileImage} alt='' />
 					<p className='comment-name'>Name</p>
 					<p className='comment-date'>1 week ago</p>
 					<p className='comment-reply'>
-						<FaArrowCircleLeft />
+						<img src={IconReply} />
 						Reply
 					</p>
 				</div>
